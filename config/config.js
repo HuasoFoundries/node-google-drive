cc5e70e290245ae219f116b9b6c100c4ff5dcb15
Amenadiel <amenadiel@gmail.com>
2018-12-07 17:51:59 -0300

    don't ignore config.js

---
+++ config/config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+require('dotenv').config({
+	path: path.resolve(__dirname + '/../.env'),
+	silent: false
+});
+
+const env = process.env; // eslint-disable-line no-process-env
+
+const Config = {
+	GOOGLE_DRIVE_ROOT_FOLDER: env.GOOGLE_DRIVE_ROOT_FOLDER,
+	GOOGLE_DRIVE_KEY_FILE: env.GOOGLE_DRIVE_KEY_FILE
+};
+
+module.exports = Config;
